8074d3596356974423e898884025ea497003efe1
Ujwal <ujjwalnagrikar@gmail.com>
2025-08-15 03:04:38 +0530

    update-tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "squid" {
   associate_public_ip_address = true
   subnet_id     = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.squid_sg.id]
-
   user_data = <<-EOF
     #!/bin/bash
               apt update -y
